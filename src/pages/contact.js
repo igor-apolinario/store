ba42de6cc292a4dc2a2954b74dfa386835e95d12
Igor Apolinário <igor.apolinario@g.globo>
2020-08-06 23:56:17 -0300

    Alterações no formulário de contato

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,16 +3,20 @@ import React from 'react'
 const Contact = () => (
   <div>
     <h1>Formulário de contato</h1>
-    <form
-      name="contact-form"
-      method="post"
-      netlify
-    >
-      <input name="name" placeholder="Your Name" type="text" />
-      <input name="email" placeholder="name@name.com" type="email" />
-      <textarea name="message" />
-      <button>Enviar</button>
-    </form>
+    <form name="contact" method="POST" data-netlify="true">
+    <p>
+      <label>Nome: <input type="text" name="name" /></label>   
+    </p>
+    <p>
+      <label>Email: <input type="email" name="email" /></label>
+    </p>
+    <p>
+      <label>Mensagem: <textarea name="message"></textarea></label>
+    </p>
+    <p>
+      <button type="submit">Enviar</button>
+    </p>
+  </form>
   </div>
 )
 
