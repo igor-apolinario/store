e098696035c3bb1a2d0e4a455b132699516a1810
Igor Apolinário <igor.apolinario@g.globo>
2020-08-06 23:59:36 -0300

    Adicionado campos ocultos ao formulário de contato

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,19 +4,21 @@ const Contact = () => (
   <div>
     <h1>Formulário de contato</h1>
     <form name="contact" method="POST" data-netlify="true">
-    <p>
-      <label>Nome: <input type="text" name="name" /></label>   
-    </p>
-    <p>
-      <label>Email: <input type="email" name="email" /></label>
-    </p>
-    <p>
-      <label>Mensagem: <textarea name="message"></textarea></label>
-    </p>
-    <p>
-      <button type="submit">Enviar</button>
-    </p>
-  </form>
+      <input type="hidden" name="bot-field" />
+      <input type="hidden" name="form-name" value="contact" />
+      <p>
+        <label>Nome: <input type="text" name="name" /></label>   
+      </p>
+      <p>
+        <label>Email: <input type="email" name="email" /></label>
+      </p>
+      <p>
+        <label>Mensagem: <textarea name="message"></textarea></label>
+      </p>
+      <p>
+        <button type="submit">Enviar</button>
+      </p>
+    </form>
   </div>
 )
 
